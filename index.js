df1a637cc8f2ae599e5d1064360e6a048cdfba9b
Cephas Mwai <cephasmwai561@gmail.com>
2025-03-19 00:49:47 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Implicitly global since it's not declared with var/let/const
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// Declare leastFavoriteCustomer as a constant in global scope
+const leastFavoriteCustomer = 'some customer';
+
+// Function to attempt changing leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another customer'; // This will throw an error
+}
